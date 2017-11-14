1d184af800f475a7f9854ea9d5174dd93cd0c8d0
mitkonikolov <nikolov.m@husky.neu.edu>
2017-11-14 18:41:40 -0500

    http request problem resolved

--- APP/app/src/main/java/com/example/li/app/DisplayResponse.java
+++ APP/app/src/main/java/com/example/li/app/DisplayResponse.java
@@ -1,5 +1,6 @@
 package com.example.li.app;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,12 +18,15 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import android.net.Uri.Builder;
 
 public class DisplayResponse extends AppCompatActivity {
-    public static final String url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze" +
-            "?visualFeatures=Faces, Categories, Tags, Description&language=en&details=Landmarks";
+    public static final String url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+            //"?visualFeatures=Faces, Categories, Tags, Description&language=en&details=Landmarks";
+    //"?model=landmarks";
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,53 +41,94 @@ public class DisplayResponse extends AppCompatActivity {
             final String subscriptionKey = "5eb78facc92b4299bd4ebc3d55d5d234";
 
 
-            JSONObject body = null;
+
             try {
-                body = new JSONObject("{\"url\":\"https://www.spaceneedle.com/wp-content/uploads/2013/11/main_03-social.jpg\"}");
-            } catch (JSONException e) {
+                Map<String, String> body = new HashMap<String, String>();
+                body.put("url","https://www.spaceneedle.com/wp-content/uploads/2013/11/main_03-social.jpg" );
+ //               body = new JSONObject("{\"url\":\"https://www.spaceneedle.com/wp-content/uploads/2013/11/main_03-social.jpg\"}");
+
+/*                Builder uriBuilder = new Builder();
+                uriBuilder.path(url);
+
+
+
+
+                // Request parameters.
+                // To use the Celebrities model, change "landmarks" to "celebrities" here and in uriBase.
+                uriBuilder.appendQueryParameter("model", "landmarks");
+
+
+
+                // Prepare the URI for the REST API call.
+                Uri uri = uriBuilder.build();*/
+
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("https")
+                        .authority("westcentralus.api.cognitive.microsoft.com")
+                        .appendPath("vision")
+                        .appendPath("v1.0")
+                        .appendPath("analyze")
+                        //.appendQueryParameter("visualFeatures", "Faces");
+                        //.appendQueryParameter("model", "landscape");
+                        .appendQueryParameter("model", "landscape");
+                        //.appendQueryParameter("sort", "relevance")
+                        //.fragment("section-name");
+                String myUrl = builder.build().toString();
+
+                Log.i("myURL is ", myUrl);
+
+
+                JsonObjectRequest request = new JsonObjectRequest(
+                        Request.Method.POST,
+                        myUrl,
+                        new JSONObject(body),
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    textView.setText("Success: " + response.toString(2));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                textView.setText("Something went wrong: " +
+                                        error.networkResponse +
+                                        "\n" + error.getStackTrace() +
+                                        "\n" + error.toString());
+                            }
+                        })
+                {
+/*                @Override
+                protected Map<String,String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    *//*params.put("visualFeatures", "Faces, Categories, Tags, Description");
+                    params.put("language", "en");
+                    params.put("details", "Landmarks");*//*
+                    params.put("model", "landmarks");
+                    return params;
+                }*/
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<String, String>();
+                        headers.put("Content-Type", "application/json; charset=utf-8");
+                        headers.put("Ocp-Apim-Subscription-Key", subscriptionKey);
+                        return headers;
+                    }
+                };
+// Add the request to the RequestQueue.
+
+                queue.add(request);
+
+            } catch (RuntimeException e) {
                 e.printStackTrace();
             }
 
 
-            JsonObjectRequest request = new JsonObjectRequest(
-                    Request.Method.POST, url, body,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                textView.setText("Success: " + response.toString(2));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            textView.setText("Something went wrong: " + error.networkResponse + "\n" + error.getStackTrace());
-                        }
-                    })
-            {
-                @Override
-                protected Map<String,String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("visualFeatures", "Faces, Categories, Tags, Description");
-                    params.put("language", "en");
-                    params.put("details", "Landmarks");
-                    return params;
-                }
-
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String> headers = new HashMap<>();
-                    headers.put("Content-Type", "application/json");
-                    headers.put("Ocp-Apim-Subscription-Key", subscriptionKey);
-                    return headers;
-                }
-            };
-// Add the request to the RequestQueue.
-
-        queue.add(request);
     }
 
 
